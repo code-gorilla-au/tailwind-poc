e8b2dccaf8fd4dec5bda48bc63ed6352dd49ea0f
mauricio riquelme <m.riquelme223@gmail.com>
2020-12-11 08:33:49 +1100

    checkbox

---
+++ src/components/BaseCheckbox.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="field">
+    <label v-show="label">{{ label }}</label>
+    <div class="checkbox-container">
+      <input
+        type="checkbox"
+        :class="['checkbox', isTouched]"
+        @focus="clearErrors"
+        @blur="handleInvlaid"
+        v-bind="$attrs"
+        :value="modelValue"
+        @input="handleInput"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import { useBaseInputs } from '@/lib/use/inputs';
+
+export default {
+  inheritAttrs: false,
+  name: 'BaseCheckbox',
+  props: {
+    label: {
+      type: String,
+      default: '',
+    },
+    modelValue: [String, Number, Boolean],
+  },
+  setup(props, ctx) {
+    const base = useBaseInputs(ctx);
+    return {
+      valid: base.valid,
+      touched: base.touched,
+      isTouched: base.isTouched,
+      invalidMessage: base.invalidMessage,
+      handleInvlaid: base.handleInvlaid,
+      handleInput: base.handleInput,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.field {
+  display: flex;
+}
+.checkbox {
+  transition: 0.3s;
+  -webkit-appearance: none;
+  background-color: var(--colour-white-default);
+  border: 1px solid var(--colour-success-default);
+  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.05), inset 0px -15px 10px -12px rgba(0, 0, 0, 0.05);
+  padding: 9px;
+  border-radius: 3px;
+  display: inline-block;
+  position: relative;
+}
+.checkbox:active .checkbox:checked:active {
+  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.05), inset 0px 1px 3px rgba(0, 0, 0, 0.1);
+}
+.checkbox:checked {
+  background-color: var(--colour-primary-default);
+  border: 1px solid var(--colour-success-default);
+  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.05), inset 0px -15px 10px -12px rgba(0, 0, 0, 0.05),
+    inset 15px 10px -12px rgba(255, 255, 255, 0.1);
+}
+.checkbox:checked::after {
+  content: '\2714';
+  font-size: 14px;
+  position: absolute;
+  top: 0px;
+  left: 3px;
+  color: var(--colour-white-default);
+}
+</style>
--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -5,7 +5,10 @@
       <BaseInput required v-model="newTodo" type="text" label="Add todo" />
     </div>
     <ul class="flex justify-center align-center">
-      <li v-for="todo in todos" :key="todo">{{ todo }}</li>
+      <li v-for="todo in todos" :key="todo.name">
+        {{ todo.name }}
+        <BaseCheckbox v-model="todo.checked" label="Done" />
+      </li>
     </ul>
   </TodoLayout>
 </template>
@@ -13,6 +16,8 @@
 <script>
 import TodoLayout from '@/layouts/TodoLayout.vue';
 import BaseInput from '@/components/BaseInput.vue';
+import BaseCheckbox from '@/components/BaseCheckbox.vue';
+
 import { ref } from 'vue';
 
 export default {
@@ -20,9 +25,10 @@ export default {
   components: {
     TodoLayout,
     BaseInput,
+    BaseCheckbox,
   },
   setup() {
-    const todos = ref(['']);
+    const todos = ref([{ name: '', checked: false }]);
     const newTodo = ref('');
     return {
       todos,
